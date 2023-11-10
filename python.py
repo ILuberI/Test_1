e78c8825c2c857b852dbfe36bdbdf9f2cbe35e90
ILuberI <leonadeu@gmail.com>
2023-11-10 19:08:39 +0900

    add py

--- python.py
+++ python.py
@@ -1,13 +1,3 @@
-def N_to_B (N, B):
-    A = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    result = []
-
-    while N > 0:
-        N, X = divmod(N, B)
-        result.append(A[X])
-
-    return "".join(result[::-1])
-
-N, B = map(int, input().split())
-result = N_to_B(N, B)
-print(result)
+N = int(input())
+result = 1 + 2**N
+print(result * result)
